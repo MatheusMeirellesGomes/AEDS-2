e70de02a601c833ccc787ed56c871bfb2c3561bd
Matheus Meirelles Gomes <matheusrmme2019@gmail.com>
2026-02-10 18:08:17 -0300

    nova atividade

--- EXERCICIOS DE JAVA/Exercicio1.java
+++ EXERCICIOS DE JAVA/Exercicio1.java
@@ -1,3 +1,4 @@
+//Apenas um exercício para imprimir uma frase, sem nenhuma entrada.
 public class Exercicio1 {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
