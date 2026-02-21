c5b84199f67463f8da70931bb3195847fb457a81
Matheus Meirelles Gomes <matheusrmme2019@gmail.com>
2026-02-21 14:50:15 -0300

    att

--- EXERCICIOS DE JAVA/Exercicio6.java
+++ EXERCICIOS DE JAVA/Exercicio6.java
@@ -1,13 +1,19 @@
-import java.util.Scanner; 
+//Exercicio para ler o nome, idade e altura de uma pessoa e imprimir essas informações formatadas. 
+import java.util.Scanner; //Biblioteca para ler a entrada do usuário.
 
+//Inicializar a classe com o mesmo nome que foi salvo no arquivo. 
 class Exercicio6 {
-    public static void main (String[] args) {
+    //Método principal onde o programa começa com a execução. 
+    public static void main (String[] args) { //Declarar as variáveis para armazenar o nome, idade e altura da pessoa. 
+        //Inicializar variáveis. 
         String nome; 
         int idade; 
         float altura; 
         
+        //Criar um objeto Scanner para ler a entrada do usuário. (como se fosse um scanf em C). 
         Scanner sc = new Scanner(System.in); 
 
+        //Ler o nome, idade e altura da pessoa do usuário e armazená-los nas variáveis correspondentes. 
         System.out.print("Informe seu nome: "); 
         nome = sc.nextLine(); 
         System.out.print("Informe sua idade: "); 
@@ -15,8 +21,10 @@ class Exercicio6 {
         System.out.print("Informe sua altura: "); 
         altura = sc.nextFloat(); 
 
+        //Imprimir as informações formatadas usando a forma correta de impressão em java. 
         System.out.println(nome+"tem "+idade+" anos e mede "+altura+" metros de altura."); 
         
+        //Fechar o scanner para evitar vazamento de memória.
         sc.close(); 
     }
 }
