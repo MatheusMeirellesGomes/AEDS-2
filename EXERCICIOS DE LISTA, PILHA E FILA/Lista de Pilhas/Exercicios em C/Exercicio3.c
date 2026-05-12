278e85ef18988bd86feb275d96e02b3f6356cff5
Matheus Gomes <MatheusGomes@MacBook-Air-de-Matheus.local>
2026-05-11 22:43:00 -0300

    att

---
+++ EXERCICIOS DE LISTA, PILHA E FILA/Lista de Pilhas/Exercicios em C/Exercicio3.c
@@ -0,0 +1,44 @@
+/*Mesmo exercício de lista de pilhas, referente ao exercício 3, porém agora na linguagem C. */
+
+#include <stdio.h> 
+#include <stdlib.h> 
+
+typedef struct Lista {
+    CelulaLista *inicio; 
+    CelulaLista *fim; 
+} Lista; 
+
+typedef struct CelulaLista {
+    CelulaPilha *topo; 
+    CelulaLista *prox; 
+} CelulaLista; 
+
+typedef struct CelulaPilha {
+    int elemento; 
+    CelulaLista *prox; 
+}
+
+//Função que retorna um inteiro
+int somaMaiorPilha (Lista *lista) {
+    //Criar variável para guardar a soma. 
+    int maiorSoma = 0; 
+
+    //Percorrer a lista 
+    for (CelulaLista *i = lista->inicio; i != NULL; i = i->prox) {
+        //Variável para a soma dos elementos da pilha. 
+        int somaAtual = 0; 
+
+        //Percorrer a pilha e ir somando os elementos. 
+        for (CelulaPilha *j = i->topo; j != NULL; j = j->prox) {
+            somaAtual += j->elemento; 
+        }
+
+        //Verificar a soma
+        if (somaAtual > maiorSoma) {
+            maiorSoma = somaAtual; 
+        }
+    }
+
+    //Retornar a maior soma
+    return maiorSoma; 
+}
